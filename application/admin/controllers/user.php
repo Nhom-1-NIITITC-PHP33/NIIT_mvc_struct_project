7b1b5da526dbfd9ab9ad95076e99f1f002c600fb
kiel <41059489+Kiel1050@users.noreply.github.com>
2018-11-07 14:51:37 +0700

    Revert "7/11/2018"
    This reverts commit 7ad6cb2176a7f2a2dddf7a9a8ac03912fd4380c2.

--- application/admin/controllers/user.php
+++ application/admin/controllers/user.php
@@ -9,36 +9,7 @@ class Default_Controllers_User extends Libs_Controller {
     public function login(){
         $this->view->render('user/login');
     }
-
-    public function test_input($data) {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
     
-    public function loginProccess() {
-        $email = $this->test_input($_POST['email']);
-        $password = $this->test_input($_POST['password']);
-
-        $database = new Libs_Model;
-        $db = $database->getConnection();
-        $employee = new Admin_Models_Employee($db);
-        $employee->email = $email;
-        $employee->password = $password;
-
-        if ($employee->checkEmployee() == true) {
-            //Caaps session
-            $_SESSION['logged_in'] = true;
-            $_SESSION['email'] = $email;
-            $_SESSION['name']=$fullName;
-            //Dieu huong
-            echo "<script>window.location.href='" . URL_BASE . "';</script>";
-        } else {
-            //redirect ve login
-            echo "<script>window.location.href='" . URL_BASE . "user/login';</script>";
-        }
-    }
    
 
 }
